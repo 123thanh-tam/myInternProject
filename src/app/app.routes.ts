b3659d9a7c4d6fcff95bff430aa78628a93734f3
haitc <hai.tc21@gmail.com>
2023-12-12 09:16:33 +0700

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,36 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { NotFoundComponent } from './error/not-found/not-found.component';
-import { HomeComponent } from './module/home/home.component';
+import { HomeComponent } from './page/home/home.component';
+import { AccessDeniedComponent } from './error/access-denied/access-denied.component';
+import { ServerErrorComponent } from './error/server-error/server-error.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: LayoutComponent,
-        children: [
-            {
-                path: '',
-                component: HomeComponent,
-            },
-            {
-                path: 'about',
-                loadChildren: () => import('./module/about/about.module').then(m => m.AboutModule),
-            },
-        ]
-    },
-    {
-        path: '**',
-        component: NotFoundComponent
-    }
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('./page/about/about.module').then((m) => m.AboutModule),
+      },
+    ],
+  },
+  {
+    path: 'access-denied',
+    component: AccessDeniedComponent,
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
